d48ecf43a4e491ca7aa6b8a60b085a6830bd60ba
Joathan Silva <jonathanmdp@gmail.com>
2020-10-28 19:08:29 -0300

    atualizacao do set_ec2_tags_zabbix_hosts_v2.py corrigido  a questão de duplicidade ao adicionar o elemento na lista.

---
+++ set_ec2_tags_zabbix_v2.py
@@ -0,0 +1,113 @@
+
+import boto3
+from zabbix_api import ZabbixAPI, ZabbixAPIException
+
+#Dictionary Teams
+team_list = {'canais': 'channel', 'produtos': 'product', 'plataformas': 'platforms'}
+host_name = ""
+tag_update= ""
+host_tag = ''
+
+# The hostname at which the Zabbix web interface is available
+ZABBIX_SERVER = 'http://192.168.1.103/zabbix'
+zapi = ZabbixAPI(ZABBIX_SERVER, timeout=180)
+
+
+#session profile for aws
+mysession = boto3.session.Session(profile_name='default')
+ec2client = boto3.client('ec2')
+
+#request instance
+response = ec2client.describe_instances()
+for reservation in response["Reservations"]:
+    for instance in reservation["Instances"]:
+        #This sample print will output entire Dictionary object
+        #print(instance)
+        # This will print will output the value of the Dictionary key 'InstanceId'
+        Tags = instance["Tags"]
+        for field in Tags:
+            if (field['Key'] == 'opsworks:instance'):
+                #print('{}'.format(field['Key']).lower())
+                zhost = ('{}'.format(field['Value']).lower())
+                host_name = zhost
+                print(zhost)
+
+            if (field['Key'].lower() == 'team'):
+                #print('{}'.format(field['Key']).lower())
+                team = ('{}'.format(field['Value']).lower())
+                tag_update=team_list[team]
+                print(tag_update)
+
+
+
+
+    try:
+
+        # Login to the Zabbix API
+        zapi.login('Admin', 'zabbix')
+        print('Conectado na API do Zabbix {}'.format(zapi.api_version()))
+
+    except Exception as err:
+        print('Falha ao conectar na API do Zabbix: {}'.format(err))
+
+    #Get the Host information in the Zabbix-Server
+    try:
+
+        hosts = zapi.host.get({
+            "output": "extend",
+            "filter": {
+                "host": [
+                    host_name
+                ]
+            },
+            "selectTags": [
+                "tag", "value"]
+        })
+        for host in hosts:
+            hosts_id = host['hostid']
+            host_name = host['host']
+            host_update = hosts_id
+            host_visiblename = host['name']
+            hos_status = host['status']
+            for xtag in host['tags']:
+                if (xtag['tag'].lower() == 'team'):
+                    host_tag = xtag['value']
+                else:
+                    old_tags = host['tags']
+                    new = {'tag': 'team', 'value': tag_update}
+                    for x in old_tags:
+                        if new not in old_tags:
+                            old_tags.append(new)
+                    tag_team = zapi.host.update({
+                    "hostid": host_update,
+                    "tags": old_tags
+                    })
+                    host_tag = tag_update
+
+            print('{} - {} - {} - {} - {}'.format(hosts_id, host_name, host_visiblename, hos_status, host_tag))
+    except ZabbixAPIException as e:
+        print(e)
+
+
+    # Update host_tag
+    if host_tag == None:
+        try:
+            old_tags = host['tags']
+            new = {'tag': 'team', 'value': tag_update}
+            for x in old_tags:
+                if new not in old_tags:
+                    old_tags.append(new)
+            #print(type(old_tags))
+            updating = zapi.host.update({
+            "hostid": host_update,
+            "tags": old_tags
+            })
+            host_tag = tag_update
+            print("Atualização realizada com sucesso")
+        except ZabbixAPIException as e:
+            print(e)
+
+    else:
+        print("Tag ja atualizada")
+
+
